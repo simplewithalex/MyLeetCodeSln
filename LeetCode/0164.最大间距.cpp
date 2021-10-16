9d32da51744745bcb0da5b8e3980926b93497cd8
sorrowfulliu <3185435464@qq.com>
2021-10-16 13:00:06 +0800

    Oct 16 2021

---
+++ LeetCode/0164.最大间距.cpp
@@ -0,0 +1,89 @@
+﻿#include "header.h"
+
+/*
+
+给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+如果数组元素个数小于 2，则返回 0
+
+示例 1:
+输入: [3,6,9,1]
+输出: 3
+解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3
+
+示例 2:
+输入: [10]
+输出: 0
+解释: 数组元素个数小于 2，因此返回 0
+
+说明:
+你可以假设数组中所有元素都是非负整数，且数值在 32 位有符号整数范围内。
+请尝试在线性时间复杂度和空间复杂度的条件下解决此问题。
+
+*/
+
+// 基数排序
+class Solution1 {
+public:
+	int maximumGap(vector<int> &nums) {
+		int len = nums.size();
+		if (len < 2) return 0;
+		int exp = 1;
+		int maxVal = *max_element(nums.begin(), nums.end());
+		vector<int> buf(len);
+		while (maxVal >= exp) {
+			vector<int> cnt(10);
+			for (int i = 0; i < len; ++i) {
+				int digit = (nums[i] / exp) % 10;
+				++cnt[digit];
+			}
+			for (int i = 1; i < 10; ++i) {
+				cnt[i] += cnt[i - 1];
+			}
+			for (int i = len - 1; i >= 0; --i) {
+				int digit = (nums[i] / exp) % 10;
+				buf[cnt[digit] - 1] = nums[i];
+				--cnt[digit];
+			}
+			copy(buf.begin(), buf.end(), nums.begin());
+			exp *= 10;
+		}
+		int res = 0;
+		for (int i = 1; i < len; ++i) {
+			res = max(res, nums[i] - nums[i - 1]);
+		}
+		return res;
+	}
+};
+// https://leetcode-cn.com/problems/maximum-gap/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by--39/
+
+// 桶排序
+class Solution2 {
+public:
+	int maximumGap(vector<int> &nums) {
+		int len = nums.size();
+		if (len < 2) return 0;
+		int minVal = *min_element(nums.begin(), nums.end());
+		int maxVal = *max_element(nums.begin(), nums.end());
+		int cap = max(1, (maxVal - minVal) / (len - 1));
+		int bucketNum = ((maxVal - minVal) / cap) + 1;
+		vector<pair<int, int>> bucket(bucketNum, { -1,-1 });
+		for (int i = 0; i < len; ++i) {
+			int idx = (nums[i] - minVal) / cap;
+			if (bucket[idx].first == -1) {
+				bucket[idx] = { nums[i],nums[i] };
+			}
+			else {
+				bucket[idx].first = min(bucket[idx].first, nums[i]);
+				bucket[idx].second = max(bucket[idx].second, nums[i]);
+			}
+		}
+		int res = 0, prev = -1;
+		for (int i = 0; i < bucketNum; ++i) {
+			if (bucket[i].first == -1) continue;
+			if (prev != -1) res = max(res, bucket[i].first - bucket[prev].second);
+			prev = i;
+		}
+		return res;
+	}
+};
+// https://leetcode-cn.com/problems/maximum-gap/solution/zui-da-jian-ju-by-leetcode-solution/
---
+++ LeetCode/0166.分数到小数.cpp
@@ -0,0 +1,63 @@
+﻿#include "header.h"
+
+/*
+
+给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以 字符串形式返回小数
+如果小数部分为循环小数，则将循环的部分括在括号内
+如果存在多个答案，只需返回 任意一个
+对于所有给定的输入，保证 答案字符串的长度小于 10^4
+
+示例 1：
+输入：numerator = 1, denominator = 2
+输出："0.5"
+
+示例 2：
+输入：numerator = 2, denominator = 1
+输出："2"
+
+示例 3：
+输入：numerator = 2, denominator = 3
+输出："0.(6)"
+
+示例 4：
+输入：numerator = 4, denominator = 333
+输出："0.(012)"
+
+示例 5：
+输入：numerator = 1, denominator = 5
+输出："0.2"
+
+提示：
+-2^31 <= numerator, denominator <= 2^31 - 1
+denominator != 0
+
+*/
+
+class Solution {
+public:
+	string fractionToDecimal(int numerator, int denominator) {
+		if (numerator == 0) return "0";
+		string decimal;
+		if ((numerator > 0) ^ (denominator > 0)) decimal.append("-");
+		// 本题目INT_MIN不太好处理，统一转为长整型
+		long long dividend = abs((long long)numerator);
+		long long divisor = abs((long long)denominator);
+		long long remainder = dividend%divisor;
+		decimal.append(to_string(dividend / divisor));
+		if (remainder == 0) return decimal;
+		decimal.append(".");
+		unordered_map<long long, int> m;
+		while (remainder != 0) {
+			if (m.count(remainder) != 0) {
+				decimal.insert(m[remainder], "(");
+				decimal.append(")");
+				break;
+			}
+			m[remainder] = decimal.size();
+			remainder *= 10;
+			decimal.append(to_string(remainder / divisor));
+			remainder %= divisor;
+		}
+		return decimal;
+	}
+};
