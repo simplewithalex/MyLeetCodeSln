05511c208e4bcab2bf9a8a821fc2dba1a7761540
sorrowfulliu <3185435464@qq.com>
2021-11-27 22:38:25 +0800

    Nov 27 2021

---
+++ LeetCode/0168.Excel表列名称.cpp
@@ -0,0 +1,56 @@
+﻿#include "header.h"
+
+/*
+
+给你一个整数 columnNumber ，返回它在Excel表中相对应的列名称。
+
+例如：
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28
+...
+
+示例 1：
+输入：columnNumber = 1
+输出："A"
+
+示例 2：
+输入：columnNumber = 28
+输出："AB"
+
+示例 3：
+输入：columnNumber = 701
+输出："ZY"
+
+示例 4：
+输入：columnNumber = 2147483647
+输出："FXSHRXW"
+
+提示：
+1 <= columnNumber <= 2^31 - 1
+
+*/
+
+class Solution {
+public:
+	string convertToTitle(int columnNumber) {
+		const int base = 26;
+		string res;
+		while (columnNumber > 0) {
+			--columnNumber;
+			res.append(1, 'A' + columnNumber % base);
+			columnNumber /= base;
+		}
+		reverse(res.begin(), res.end());
+		return res;
+	}
+};
+/*
+columnNumber % 26 的结果中 0 - Z , 1 - A , 2 - B, ... , 25 - Y
+显然应该是0 - A, ..., 25 - Z 更好, 所以进行columnNumber - 1 偏移操作
+*/
---
+++ LeetCode/0171.Excel 表列序号.cpp
@@ -0,0 +1,53 @@
+﻿#include "header.h"
+
+/*
+
+给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回该列名称对应的列序号。
+
+例如，
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28
+...
+
+示例 1:
+输入: columnTitle = "A"
+输出: 1
+
+示例 2:
+输入: columnTitle = "AB"
+输出: 28
+
+示例 3:
+输入: columnTitle = "ZY"
+输出: 701
+
+示例 4:
+输入: columnTitle = "FXSHRXW"
+输出: 2147483647
+
+提示：
+1 <= columnTitle.length <= 7
+columnTitle 仅由大写英文组成
+columnTitle 在范围 ["A", "FXSHRXW"] 内
+
+*/
+
+
+class Solution {
+public:
+	int titleToNumber(string columnTitle) {
+		int res = 0;
+		unsigned int mul = 1;
+		for (int i = columnTitle.size() - 1; i >= 0; --i) {
+			res += (columnTitle[i] - 'A' + 1)*mul;
+			mul *= 26;
+		}
+		return res;
+	}
+};
