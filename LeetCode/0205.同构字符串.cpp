7d404e1414461531603511a678bd96420580aa04
sorrowfulliu <3185435464@qq.com>
2022-01-09 17:09:14 +0800

    Jan 09 2022

---
+++ LeetCode/0205.同构字符串.cpp
@@ -0,0 +1,70 @@
+﻿#include "header.h"
+
+/*
+
+给定两个字符串 s 和 t，判断它们是否是同构的。
+
+如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
+每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。
+不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+
+示例 1:
+输入：s = "egg", t = "add"
+输出：true
+
+示例 2：
+输入：s = "foo", t = "bar"
+输出：false
+
+示例 3：
+输入：s = "paper", t = "title"
+输出：true
+  
+提示：可以假设 s 和 t 长度相同。
+
+*/
+
+class Solution1 {
+public:
+	bool isIsomorphic(string s, string t) {
+		return helper(s, t) && helper(t, s);
+	}
+
+private:
+	bool helper(const string &s, const string &t) {
+		int len = s.size();
+		vector<char> bkt(128, -1);
+		for (int i = 0; i < len; ++i) {
+			if (bkt[s[i]] != -1) {
+				if (bkt[s[i]] != t[i]) return false;
+			}
+			else {
+				bkt[s[i]] = t[i];
+			}
+		}
+		return true;
+	}
+};
+
+
+class Solution2 {
+public:
+	bool isIsomorphic(string s, string t) {
+		vector<char> bkt_s(128, -1);
+		vector<char> bkt_t(128, -1);
+		int len = s.size();
+		for (int i = 0; i < len; ++i) {
+			if (bkt_s[s[i]] != bkt_t[t[i]]) {
+				return false;
+			}
+			else {
+				if (bkt_s[s[i]] == -1) {
+					bkt_s[s[i]] = i;
+					bkt_t[t[i]] = i;
+				}
+			}
+		}
+		return true;
+	}
+};
+// https://leetcode-cn.com/problems/isomorphic-strings/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-42/
---
+++ LeetCode/0209.长度最小的子数组.cpp
@@ -0,0 +1,97 @@
+﻿#include "header.h"
+
+/*
+
+给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+找出该数组中满足其和 ≥ target 的长度最小的连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。
+如果不存在符合条件的子数组，返回 0 。
+
+示例 1：
+输入：target = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+
+示例 2：
+输入：target = 4, nums = [1,4,4]
+输出：1
+
+示例 3：
+输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+输出：0
+ 
+提示：
+1 <= target <= 10^9
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^5
+
+*/
+
+// 暴力搜索
+class Solution1 {
+public:
+	int minSubArrayLen(int target, vector<int> &nums) {
+		int len = nums.size();
+		int res = INT_MAX;
+		for (int i = 0; i < len; ++i) {
+			int sum = 0;
+			for (int j = i; j < len; ++j) {
+				sum += nums[j];
+				if (sum >= target) {
+					res = min(res, j - i + 1);
+					break;
+				}
+			}
+		}
+		return res == INT_MAX ? 0 : res;
+	}
+};
+
+// 前缀和 + 二分
+class Solution2 {
+public:
+	int minSubArrayLen(int target, vector<int> &nums) {
+		int len = nums.size();
+		vector<int> sums(len + 1, 0);
+		int res = INT_MAX;
+		for (int i = 1; i <= len; ++i) sums[i] = sums[i - 1] + nums[i - 1];
+		for (int i = 1; i <= len; ++i) {
+			int tar = sums[i - 1] + target;
+			int lo_bound = lowerBound(sums, i, len, tar);
+			if (lo_bound != -1) res = min(res, lo_bound - i + 1);
+		}
+		return res == INT_MAX ? 0 : res;
+	}
+
+private:
+	int lowerBound(vector<int> &nums, int lo, int hi, int tar) {
+		while (lo < hi) {
+			int mi = lo + (hi - lo) / 2;
+			if (nums[mi] < tar) lo = mi + 1;
+			else hi = mi;
+		}
+		return nums[lo] >= tar ? lo : -1;
+	}
+};
+// https://leetcode-cn.com/problems/minimum-size-subarray-sum/solution/chang-du-zui-xiao-de-zi-shu-zu-by-leetcode-solutio/
+
+// 双指针滑窗
+class Solution3 {
+public:
+	int minSubArrayLen(int target, vector<int> &nums) {
+		int len = nums.size();
+		int res = INT_MAX;
+		int start = 0, end = 0;
+		int sum = 0;
+		while (end < len) {
+			sum += nums[end];
+			while (sum >= target) {
+				res = min(res, end - start + 1);
+				sum -= nums[start];
+				++start;
+			}
+			++end;
+		}
+		return res == INT_MAX ? 0 : res;
+	}
+};
