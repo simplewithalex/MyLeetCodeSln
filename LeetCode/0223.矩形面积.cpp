e7f75e7f7222869a2556b57f8d7cca8f52081483
sorrowfulliu <3185435464@qq.com>
2022-03-06 12:58:53 +0800

    Mar 06 2022

---
+++ LeetCode/0223.矩形面积.cpp
@@ -0,0 +1,33 @@
+﻿#include "header.h"
+
+/*
+
+给你二维平面上两个由直线构成且边与坐标轴平行/垂直的矩形，请你计算并返回两个矩形覆盖的总面积。
+
+每个矩形由其 左下 顶点和 右上 顶点坐标表示：
+第一个矩形由其左下顶点 (ax1, ay1) 和右上顶点 (ax2, ay2) 定义。
+第二个矩形由其左下顶点 (bx1, by1) 和右上顶点 (bx2, by2) 定义。
+ 
+示例 1：
+输入：ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2
+输出：45
+ 
+示例 2：
+输入：ax1 = -2, ay1 = -2, ax2 = 2, ay2 = 2, bx1 = -2, by1 = -2, bx2 = 2, by2 = 2
+输出：16
+  
+提示：
+-10^4 <= ax1, ay1, ax2, ay2, bx1, by1, bx2, by2 <= 10^4
+
+*/
+
+class Solution {
+public:
+	int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+		int area1 = (ax2 - ax1)*(ay2 - ay1), area2 = (bx2 - bx1)*(by2 - by1);
+		int overlapWidth = min(ax2, bx2) - max(ax1, bx1);
+		int overlapHeight = min(ay2, by2) - max(ay1, by1);
+		int overlapArea = max(overlapWidth, 0)*max(overlapHeight, 0);
+		return area1 + area2 - overlapArea;
+	}
+};
