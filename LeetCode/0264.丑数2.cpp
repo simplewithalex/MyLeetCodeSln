92a9e98d561c76c21bcd5a7ab1a44c7568b56a9e
sorrowfulliu <3185435464@qq.com>
2022-05-14 21:24:56 +0800

    May 14 2022

---
+++ LeetCode/0264.丑数2.cpp
@@ -0,0 +1,63 @@
+﻿#include "header.h"
+
+/*
+
+给你一个整数 n ，请你找出并返回第 n 个丑数。丑数就是只包含质因数 2、3 和/或 5 的正整数。
+
+示例 1：
+输入：n = 10
+输出：12
+解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+
+示例 2：
+输入：n = 1
+输出：1
+解释：1 通常被视为丑数。
+  
+提示：1 <= n <= 1690
+
+*/
+
+// 优先级队列
+class Solution1 {
+public:
+	int nthUglyNumber(int n) {
+		vector<int> factors{ 2,3,5 };
+		unordered_set<long long> s;
+		priority_queue<long long, vector<long long>, greater<long long>> pq;
+		s.insert(1LL);
+		pq.push(1LL);
+		int ugly = 0;
+		for (int i = 0; i < n; ++i) {
+			long long cur = pq.top();
+			pq.pop();
+			ugly = cur;
+			for (int factor : factors) {
+				long long next = cur*factor;
+				if (!s.count(next)) {
+					s.insert(next);
+					pq.push(next);
+				}
+			}
+		}
+		return ugly;
+	}
+};
+
+// 多路归并
+class Solution2 {
+public:
+	int nthUglyNumber(int n) {
+		vector<int> ugly(n, 0);
+		ugly[0] = 1;
+		int p2 = 0, p3 = 0, p5 = 0;
+		for (int i = 1; i < n; ++i) {
+			int u2 = ugly[p2] * 2, u3 = ugly[p3] * 3, u5 = ugly[p5] * 5;
+			ugly[i] = min(u2, min(u3, u5));
+			if (ugly[i] == u2) ++p2;
+			if (ugly[i] == u3) ++p3;
+			if (ugly[i] == u5) ++p5;
+		}
+		return ugly[n - 1];
+	}
+};
---
+++ LeetCode/0313.超级丑数.cpp
@@ -0,0 +1,49 @@
+﻿#include "header.h"
+
+/*
+
+超级丑数是一个正整数，并满足其所有质因数都出现在质数数组 primes 中。给你一个整数 n 和一个整数数组 primes ，返回第 n 个 超级丑数
+题目数据保证第 n 个 超级丑数 在 32-bit 带符号整数范围内
+
+示例 1：
+输入：n = 12, primes = [2,7,13,19]
+输出：32
+解释：给定长度为 4 的质数数组 primes = [2,7,13,19]，前 12 个超级丑数序列为：[1,2,4,7,8,13,14,16,19,26,28,32] 。
+
+示例 2：
+输入：n = 1, primes = [2,3,5]
+输出：1
+解释：1 不含质因数，因此它的所有质因数都在质数数组 primes = [2,3,5] 中。
+
+提示：
+1 <= n <= 10^6
+1 <= primes.length <= 100
+2 <= primes[i] <= 1000
+题目数据保证 primes[i] 是一个质数
+primes中的所有值都互不相同 ，且按递增顺序排列
+
+*/
+
+// 参考0264，多路归并
+class Solution {
+public:
+	int nthSuperUglyNumber(int n, vector<int> &primes) {
+		vector<int> ugly(n);
+		int len = primes.size();
+		vector<int> p(len);
+		vector<long long> nums(len);
+		ugly[0] = 1;
+		for (int i = 1; i < n; ++i) {
+			long long minNum = INT_MAX;
+			for (int j = 0; j < len; ++j) {
+				nums[j] = ugly[p[j]] * primes[j];
+				minNum = min(minNum, nums[j]);
+			}
+			ugly[i] = minNum;
+			for (int j = 0; j < len; ++j) {
+				if (ugly[i] == nums[j]) ++p[j];
+			}
+		}
+		return ugly[n - 1];
+	}
+};
