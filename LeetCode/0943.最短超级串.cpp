ab653a46bb2afe699d28f9c42a1f2c87c880df6b
sorrowfulliu <3185435464@qq.com>
2024-04-06 20:14:08 +0800

    Apr 05 2024

--- LeetCode/0943.最短超级串.cpp
+++ LeetCode/0943.最短超级串.cpp
@@ -26,7 +26,8 @@ words 中的所有字符串 互不相同
 
 */
 
-class Solution {
+// 备忘录
+class Solution1 {
 private:
 	vector<vector<int>> memo;
 	vector<vector<int>> prev;
@@ -103,4 +104,82 @@ private:
 };
 // https://leetcode.cn/problems/find-the-shortest-superstring/solutions/2278271/943-zui-duan-chao-ji-chuan-by-stormsunsh-w594/
 // @author https://leetcode.cn/u/stormsunshine/
+
+
+
+// 自底而上的动态规划
+class Solution2 {
+public:
+	string shortestSuperstring(vector<string> &words) {
+		int len = words.size();
+		vector<vector<int>> overlaps(len, vector<int>(len, 0));
+		for (int i = 0; i < len; ++i) {
+			string word1 = words[i];
+			for (int j = 0; j < len; ++j) {
+				string word2 = words[j];
+				if (i == j) overlaps[i][j] = word1.size();
+				else overlaps[i][j] = getOverlap(word1, word2);
+			}
+		}
+		int total = 1 << len;
+		vector<vector<int>> dp(total, vector<int>(len, INT_MAX));
+		vector<vector<int>> prev(total, vector<int>(len, -1));
+		for (int i = 1; i < total; ++i) {
+			for (int j = 0; j < len; ++j) {
+				if (i & (1 << j)) {
+					int wordLen = words[j].size();
+					int preState = i - (1 << j);
+					if (preState == 0) {
+						dp[i][j] = wordLen;
+					} else {
+						for (int k = 0; k < len; ++k) {
+							if (preState & (1 << k)) {
+								int curLen =
+									dp[preState][k] + wordLen - overlaps[k][j];
+								if (curLen < dp[i][j]) {
+									dp[i][j] = curLen;
+									prev[i][j] = k;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		int minLen = dp[total - 1][0];
+		int minIdx = 0;
+		for (int j = 1; j < len; ++j) {
+			if (dp[total - 1][j] < minLen) {
+				minLen = dp[total - 1][j];
+				minIdx = j;
+			}
+		}
+
+		vector<int> permutation(len);
+		for (int state = total - 1, i = minIdx, j = len - 1; j >= 0; --j) {
+			permutation[j] = i;
+			int preIdx = prev[state][i];
+			state -= (1 << i);
+			i = preIdx;
+		}
+
+		string res = words[permutation[0]];
+		for (int i = 1; i < len; ++i) {
+			int overlap = overlaps[permutation[i - 1]][permutation[i]];
+			res.append(words[permutation[i]].substr(overlap));
+		}
+		return res;
+	}
+
+private:
+	int getOverlap(const string &word1, const string &word2) {
+		int maxLen = min(word1.size(), word2.size());
+		for (int i = maxLen; i >= 1; --i) {
+			if (word1.substr(word1.size() - i) == word2.substr(0, i)) {
+				return i;
+			}
+		}
+		return 0;
+	}
+};
 
