c34f4147149c09e893fc99a80959ca3b83639904
sorrowfulliu <3185435464@qq.com>
2025-10-06 14:12:15 +0800

    Oct 06 2025

--- LeetCode/0139.单词拆分1.cpp
+++ LeetCode/0139.单词拆分1.cpp
@@ -60,3 +60,55 @@ public:
 		return conc[len];
 	}
 };
+
+
+// 灵神的方法
+// 记忆化
+class Solution3 {
+public:
+	vector<int> memo;
+	unordered_set<string> set;
+	int max_length = 0;
+	bool wordBreak(string s, vector<string> &wordDict) {
+		set.insert(wordDict.begin(), wordDict.end());
+		for (const auto &str : wordDict) max_length = max(max_length, (int)str.length());
+		int len = s.size();
+		memo.resize(len + 1, -1);
+		return helper(s, len);
+	}
+	bool helper(string &s, int i) {
+		if (i == 0) return true;
+		if (memo[i] != -1) return memo[i];
+		for (int j = i - 1; j >= max(i - max_length, 0); --j) {
+			if (set.count(s.substr(j, i - j)) && helper(s, j)) return memo[i] = true;
+		}
+		return memo[i] = false;
+	}
+};
+
+// 动态规划
+class Solution4 {
+public:
+	bool wordBreak(string s, vector<string> &wordDict) {
+		unordered_set<string> set(wordDict.begin(), wordDict.end());
+		int max_length = 0;
+		for (const auto &str : wordDict) max_length = max(max_length, (int)str.length());
+		int len = s.size();
+		vector<int> dp(len + 1);
+		dp[0] = true;
+		for (int i = 1; i <= len; ++i) {
+			for (int j = i - 1; j >= max(i - max_length, 0); --j) {
+				if (set.count(s.substr(j, i - j)) && dp[j]) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		return dp[len];
+	}
+};
+
+// https://leetcode.cn/problems/word-break/solutions/2968135/jiao-ni-yi-bu-bu-si-kao-dpcong-ji-yi-hua-chrs/
+// @author https://leetcode.cn/u/endlesscheng/
+
+
--- LeetCode/0140.单词拆分2.cpp
+++ LeetCode/0140.单词拆分2.cpp
@@ -22,7 +22,7 @@ wordDict = ["cat", "cats", "and", "sand", "dog"]
 
 */
 
-//该算法使用了记忆化回溯，可以避免重复递归，回溯前可以先使用单词拆分1中的算法检查是否可拆分。
+// 该算法使用了记忆化回溯，可以避免重复递归，回溯前可以先使用 单词拆分 1 中的算法检查是否可拆分。
 class Solution1 {
 public:
 	vector<string> wordBreak(string s, vector<string> &wordDict)
@@ -77,7 +77,7 @@ public:
 	}
 };
 
-//使用普通回溯，通过对比可以看出记忆化回溯的优势。
+// 使用普通回溯，通过对比可以看出记忆化回溯的优势。
 class Solution2 {
 public:
 	vector<string> wordBreak(string s, vector<string> &wordDict)
@@ -128,3 +128,71 @@ public:
 		return conc[len];
 	}
 };
+
+
+// 借鉴 单词拆分 1 中灵神的方法
+// 记忆化
+class Solution3 {
+public:
+	unordered_set<string> set;
+	unordered_map<int, vector<string>> map;
+	int max_length = 0;
+	vector<string> wordBreak(string s, vector<string> &wordDict) {
+		set.insert(wordDict.begin(), wordDict.end());
+		for (const auto &str : wordDict) max_length = max(max_length, (int)str.length());
+		int len = s.size();
+		helper(s, len);
+		return map[len];
+	}
+	void helper(string &s, int i) {
+		if (map.count(i)) return;
+		map[i] = {};
+		if (i == 0) {
+			map[i].push_back("");
+			return;
+		}
+		for (int j = i - 1; j >= max(i - max_length, 0); --j) {
+			string word = s.substr(j, i - j);
+			if (set.count(word)) {
+				helper(s, j);
+				for (const string &prefix : map[j]) {
+					map[i].push_back(prefix.empty() ? word
+													: prefix + " " + word);
+				}
+			}
+		}
+	}
+};
+
+// 动态规划
+class Solution4 {
+public:
+	vector<string> wordBreak(string s, vector<string> &wordDict) {
+		unordered_set<string> set(wordDict.begin(), wordDict.end());
+		int max_length = 0;
+		for (const auto &str : wordDict) max_length = max(max_length, (int)str.length());
+		int len = s.size();
+		unordered_map<int, vector<string>> dp;
+		dp[0] = { "" };
+		for (int i = 1; i <= len; ++i) {
+			dp[i] = {};
+			for (int j = i - 1; j >= max(i - max_length, 0); --j) {
+				string word = s.substr(j, i - j);
+				if (set.count(word)) {
+					for (const string &prefix : dp[j]) {
+						dp[i].push_back(prefix.empty() ? word
+													   : prefix + " " + word);
+					}
+				}
+			}
+		}
+		return dp[len];
+	}
+};
+// 这个方法对于某些case有缺陷，比如：
+// s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+// wordDict = ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]
+// 这个case实际上 i下标 不会遍历到 0，在中间某个值 不符条件 就可以返回了，剩下的可以全部剪掉，所以 记忆化 优于 自底而上的动态规划
+
+
+
